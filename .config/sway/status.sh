fde4a2ad521ab09c775fac66c144e75640239883
Robert Barnes <robertbarnes1981@googlemail.com>
2020-06-24 13:57:12 +0100

    Add temperature, swap voltage for capacity

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -5,6 +5,9 @@ battery_status=$(cat /sys/class/power_supply/BAT0/status)
 host=$(hostname)
 kernel=$(uname -s)
 cpu=$(top -n 1 -b -i | grep 'Cpu(s)' | awk -F ' ' '{print $2}')
+temp=$(sensors acpitz-acpi-0 | grep 'temp1' | awk -F ' ' '{print $2}')
+#volts=$(sensors BAT0-acpi-0 | grep 'in0' | awk -F ' ' '{print $2}')
+capacity=$(cat /sys/class/power_supply/BAT0/capacity)
 
-echo -e "$kernel $linux_version \U1f427 $cpu% \U1f4bb $battery_status \U1f50b $date_formatted"
+echo -e "$kernel $linux_version \U1f427 $cpu% $temp \U1f4bb $battery_status $capacity% \U1f50b $date_formatted"
 
